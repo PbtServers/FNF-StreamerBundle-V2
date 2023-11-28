6e98a851ac94a239fc50951d7ed72bf9c7d41dc0
PbtinaDev <pbt@pbtservers.com>
2023-11-28 02:49:53 +0100

    Primera Versión de "PsychoStream" (No Terminada)

---
+++ example_mods/psychostream/data/alertacubica/Script.lua
@@ -0,0 +1,9 @@
+function onUpdate()
+
+	if curBeat == 2064 then
+
+		// idk lol
+
+	end
+
+end
---
+++ example_mods/psychostream/stages/psychostage.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('bgcasaterror', 'bgcasaterror', -600, -300);
+	setScrollFactor('bgcasaterror', 0.9, 0.9);
+
+	addLuaSprite('bgcasaterror', false);
+end
