742b9d8f251f81d944b61f73dd0f0938036ede6f
PbtinaDev <pbt@pbtservers.com>
2024-02-04 11:34:38 +0100

    Versión 2.0.0 (NW) de StreamerBundle (Cambio de Assets)

--- example_mods/momentazo/stages/flumpterstage.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-	-- background shit
-	makeLuaSprite('bgflumpter', 'bgflumpter', -600, -300);
-	setScrollFactor('bgflumpter', 0.9, 0.9);
-
-	addLuaSprite('bgflumpter', false);
-end
