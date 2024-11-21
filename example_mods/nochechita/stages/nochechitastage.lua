138b57ba2782fc35de481247d9e806bd4f4afb59
PbtinaDev <pbt@pbtservers.com>
2024-11-21 18:11:22 +0100

    Nochechita Finalizada...

---
+++ example_mods/nochechita/stages/nochechitastage.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('bgnochechita', 'bgnochechita', -600, -300);
+	setScrollFactor('bgnochechita', 0.9, 0.9);
+
+	addLuaSprite('bgnochechita', false);
+end
