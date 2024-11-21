16ae35af4bc4dd5c2996394524dd3f2550d5444d
PbtinaDev <pbt@pbtservers.com>
2024-11-21 14:43:35 +0100

    DespertadOR Finalizado...

---
+++ example_mods/despertador/stages/despertadORstage.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('bgdespertadOR', 'bgdespertadOR', -600, -300);
+	setScrollFactor('bgdespertadOR', 0.9, 0.9);
+
+	addLuaSprite('bgdespertadOR', false);
+end
