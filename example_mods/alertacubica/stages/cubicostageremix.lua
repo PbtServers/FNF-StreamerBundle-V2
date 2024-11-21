6abadeb1492df561c67af63d0a83fe5ad0ab037e
PbtinaDev <pbt@pbtservers.com>
2024-11-21 13:46:46 +0100

    Remix de AlertaCubica Finalizado...

---
+++ example_mods/alertacubica/stages/cubicostageremix.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('bgcubicoremix', 'bgcubicoremix', -600, -300);
+	setScrollFactor('bgcubicoremix', 0.9, 0.9);
+
+	addLuaSprite('bgcubicoremix', false);
+end
