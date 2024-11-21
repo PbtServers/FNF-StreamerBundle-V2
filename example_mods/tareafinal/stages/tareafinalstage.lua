0430023c2e92cabff0fd50e8a62efe16a6ca5b00
PbtinaDev <pbt@pbtservers.com>
2024-11-21 01:01:39 +0100

    Primera implementación de TareaFinal...

---
+++ example_mods/tareafinal/stages/tareafinalstage.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('bgtareafinal', 'bgtareafinal', -600, -300);
+	setScrollFactor('bgtareafinal', 0.9, 0.9);
+
+	addLuaSprite('bgtareafinal', false);
+end
