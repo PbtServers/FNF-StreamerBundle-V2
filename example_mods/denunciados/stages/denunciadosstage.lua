07c7d93dad3b867886bca58f4c4b26832730c0b2
PbtinaDev <pbt@pbtservers.com>
2024-11-22 17:31:14 +0100

    Denunciados Finalizado...

---
+++ example_mods/denunciados/stages/denunciadosstage.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('bgdenunciados', 'bgdenunciados', -600, -300);
+	setScrollFactor('bgdenunciados', 0.9, 0.9);
+
+	addLuaSprite('bgdenunciados', false);
+end
