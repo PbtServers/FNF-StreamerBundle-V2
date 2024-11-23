64743575e2223fb4905742dac17c9c8b70f62e87
PbtinaDev <pbt@pbtservers.com>
2024-11-23 17:04:14 +0100

    ConspiraTrans Finalizada...

---
+++ example_mods/conspiratrans/stages/stageconspiratrans.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('bgconspiratrans', 'bgconspiratrans', -600, -300);
+	setScrollFactor('bgconspiratrans', 0.9, 0.9);
+
+	addLuaSprite('bgconspiratrans', false);
+end
